cd25f645023eccb35c408c54e693f259198f4639
GIGABYTE <eo.ursuz@gmail.com>
2023-03-03 22:32:06 +0300

    added Enum SortBy and merge sort realization

--- src/main/java/com/frizzer/ballsort/Main.java
+++ src/main/java/com/frizzer/ballsort/Main.java
@@ -5,13 +5,29 @@ import com.frizzer.ballsort.entity.Ball;
 import com.frizzer.ballsort.entity.BallType;
 import com.frizzer.ballsort.entity.BasketballBall;
 import com.frizzer.ballsort.entity.Color;
+import com.frizzer.ballsort.entity.FootballBall;
+import com.frizzer.ballsort.service.SortService;
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
 
   public static void main(String[] args) {
+    SortService sortService = new SortService();
+    Ball basketBall = new BasketballBall(12,new Color("red","deep"), BallType.BASKETBALL);
+    Ball basketBall1 = new BasketballBall(15,new Color("red","deer"), BallType.BASKETBALL);
+    Ball basketBall2 = new FootballBall(10,new Color("red","dek"), BallType.FOOTBALL);
+    List<Ball> balls= new ArrayList<>();
+    balls.add(basketBall);
+    balls.add(basketBall1);
+    balls.add(basketBall2);
 
-    Ball basketBall = new BasketballBall(10,new Color("red","deep"), BallType.BASKETBALL);
-    System.out.println(basketBall);
+    balls.forEach(System.out::println);
+    sortService.byType().mergeSort(balls);
+    System.out.println("After sort");
+    balls.forEach(System.out::println);
+
+
 
   }
 }
--- src/main/java/com/frizzer/ballsort/service/SortService.java
+++ src/main/java/com/frizzer/ballsort/service/SortService.java
@@ -1,6 +1,101 @@
 package com.frizzer.ballsort.service;
 
+import com.frizzer.ballsort.entity.Ball;
+import java.util.ArrayList;
+import java.util.List;
+
 public class SortService {
 
+  private SortBy sortBy = SortBy.SIZE;
 
+  public SortService bySize() {
+    sortBy = SortBy.SIZE;
+    return this;
+  }
+
+  public SortService byColor() {
+    sortBy = SortBy.COLOR;
+    return this;
+  }
+
+  public SortService byType() {
+    sortBy = SortBy.TYPE;
+    return this;
+  }
+
+  public List<Ball> mergeSort(List<Ball> balls) {
+
+    List<Ball> left = new ArrayList<Ball>();
+    List<Ball> right = new ArrayList<Ball>();
+    int center;
+
+    if (balls.size() == 1) {
+      return balls;
+    } else {
+      center = balls.size() / 2;
+      for (int i = 0; i < center; i++) {
+        left.add(balls.get(i));
+      }
+      for (int i = center; i < balls.size(); i++) {
+        right.add(balls.get(i));
+      }
+      left = mergeSort(left);
+      right = mergeSort(right);
+      merge(left, right, balls);
+    }
+    return balls;
+  }
+
+  public int compare(Ball leftBall, Ball rightBall) {
+    switch (sortBy) {
+      case SIZE -> {
+        return leftBall.getSize() - rightBall.getSize();
+      }
+      case TYPE -> {
+        return leftBall.getType().compareTo(rightBall.getType());
+      }
+      case COLOR -> {
+        return leftBall.getColor().compareTo(rightBall.getColor());
+      }
+    }
+    return 0;
+  }
+
+  public void merge(List<Ball> left, List<Ball> right, List<Ball> balls) {
+    int leftIndex = 0;
+    int rightIndex = 0;
+    int wholeIndex = 0;
+
+    while (leftIndex < left.size() && rightIndex < right.size()) {
+      if (compare(left.get(leftIndex), right.get(rightIndex)) < 0) {
+        balls.set(wholeIndex, left.get(leftIndex));
+        leftIndex++;
+      } else {
+        balls.set(wholeIndex, right.get(rightIndex));
+        rightIndex++;
+      }
+      wholeIndex++;
+    }
+
+    List<Ball> rest;
+    int restIndex;
+    if (leftIndex >= left.size()) {
+      rest = right;
+      restIndex = rightIndex;
+    } else {
+      rest = left;
+      restIndex = leftIndex;
+    }
+
+    for (int i = restIndex; i < rest.size(); i++) {
+      balls.set(wholeIndex, rest.get(i));
+      wholeIndex++;
+    }
+  }
+}
+
+enum SortBy {
+  SIZE,
+  COLOR,
+  TYPE
 }
